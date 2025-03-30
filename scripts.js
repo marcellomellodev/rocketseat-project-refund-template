24acd2358b5dbdbe673441f62d6cd98d3ba427d4
Marcelo Melo <marcellomellodev@gmail.com>
2025-03-30 15:53:38 +0200

    add or remove item from expense list

--- scripts.js
+++ scripts.js
@@ -85,8 +85,14 @@ function expenseAdd(newExpense) {
     expenseAmount.classList.add("expense-amount")
     expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`
 
+    //cria o icone de remover
+    const removeIcon = document.createElement("img")
+    removeIcon.classList.add("remove-icon")
+    removeIcon.setAttribute("src","img/remove.svg")
+    removeIcon.setAttribute("alt", "remover")
+
     //adiciona as informações do item
-    expenseItem.append(expenseIcon, expenseInfo, expenseAmount)
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
 
     //Adiciona o item na lista
     expenseList.append(expenseItem)
