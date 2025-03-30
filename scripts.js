f432dc017837d63d6007bb8304a63e3ab956aab7
Marcelo Melo <marcellomellodev@gmail.com>
2025-03-30 17:07:46 +0200

    add event remove item from list

--- scripts.js
+++ scripts.js
@@ -160,3 +160,17 @@ function updateTotals(){
     alert("Não foi possível atualizar os totais")
   }
 }
+
+// Evento que captura o clique no icone de remover
+expenseList.addEventListener("click", function(event){
+  // verifica se o elemento clicado é o icone de remover
+  if(event.target.classList.contains("remove-icon")){
+    //Obtém a li pai do elemento clicado
+    const item = event.target.closest(".expense")
+
+    //Remove o item da lista
+    item.remove()
+  }
+  //Atualiza os totais
+  updateTotals()
+})
