70b4e3817c1c9a13aafc5e1e8489c5caa6cf0d79
Marcelo Melo <marcellomellodev@gmail.com>
2025-03-30 13:51:35 +0200

    adjust the "amount" field to the default currency value

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
-console.log("Hello, World!00");
+//seleciona os elementos do formulário
+const amount = document.getElementById('amount');
+
+//captura o evento de input para formatar o valor
+amount.oninput= () => {
+  //obtem o valor atual do input e remove os caracteres não numéricos
+ let value = amount.value.replace(/\D/g, '');
+
+ //transforma o valor em centavos
+  value = Number(value) / 100;
+
+ //Atualiza o valor do input
+ amount.value = formatCurrencyBRL(value);
+}
+
+function formatCurrencyBRL(value) {
+  // Converte para número e formata como moeda
+  value = value.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  });
+
+  //retorna o valor formatado
+  return value;
+}
