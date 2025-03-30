4d251e707ae7fcd551732839f41e875f4701bcc8
Marcelo Melo <marcellomellodev@gmail.com>
2025-03-30 14:03:27 +0200

    Creates an object with the details of the new expense

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 //seleciona os elementos do formulário
+const form = document.querySelector('form');
 const amount = document.getElementById('amount');
+const expense = document.getElementById('expense');
+const category = document.getElementById('category');
 
 //captura o evento de input para formatar o valor
 amount.oninput= () => {
@@ -22,4 +25,27 @@ function formatCurrencyBRL(value) {
 
   //retorna o valor formatado
   return value;
+}
+
+//captura o evento de submit do formulário para obter os valores
+form.onsubmit = (event) => { 
+  //previne o comportamento padrão do formulário de recarregar a página
+  event.preventDefault();
+
+  //Cria um objeto com os detalhes da nova despesa
+  const newExpense = {
+    //id é gerado a partir do timestamp atual
+    id: new Date().getTime(),
+    //expense é o valor do input de despesa
+    expense: expense.value,
+    //category_id é o valor do select de categoria
+    category_id: category.value,
+    //category_name é o texto do select de categoria
+    category_name: category.options[category.selectedIndex].text,
+    //amount é o valor do input de valor
+    amount: amount.value,
+    created_at: new Date().toLocaleDateString('pt-BR'),
+  }
+
+  console.log(newExpense);
 }
